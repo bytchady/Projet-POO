139e907d170c61938e860a816c9a8b7b7b5ac1cc
bytchady <leilamahadi18@gmail.com>
2023-11-29 00:02:04 +0100

    <envoie articlev2 vers leila>

--- Project2/Article.cpp
+++ Project2/Article.cpp
@@ -1,56 +1,55 @@
 #include "Article.h"
-using namespace System;
 
-namespace NS_Article {
+namespace NS_article {
     void Article::setRefArticle(int ref_article) {
-        this->Ref_Article = ref_article;
+        Ref_Article = ref_article;
     }
 
-    void Article::setNom(String^ nom_article) {
-        this->Nom_Article = nom_article;
+    void Article::setNom(string& nom_article) {
+        Nom_Article = nom_article;
     }
 
-    void Article::setNature(String^ nature) {
-        this->Nature_Article = nature;
+    void Article::setNature(string& nature) {
+        Nature_Article = nature;
     }
 
-    void Article::setCouleur(String^ couleur) {
-        this->Couleur_Article = couleur;
+    void Article::setCouleur(string& couleur) {
+        Couleur_Article = couleur;
     }
 
     void Article::setStock(int stock) {
-        this->Stock_Article = stock;
+        Stock_Article = stock;
     }
 
     void Article::setQuantiteReapprovisionnement(int quantite_reapprovisionnement) {
-        this->Quantite_Reapprovisionnement = quantite_reapprovisionnement;
+        Quantite_Reapprovisionnement = quantite_reapprovisionnement;
     }
 
     void Article::setPrixHT(float prix_ht) {
-        this->Prix_HT = prix_ht;
+        Prix_HT = prix_ht;
     }
 
     void Article::setTauxTVA(float taux_tva) {
-        this->Taux_TVA = taux_tva;
+        Taux_TVA = taux_tva;
     }
 
     void Article::setSupprimer(bool supprimer) {
-        this->Supprimer = supprimer;
+        Supprimer = supprimer;
     }
 
     int Article::getRefArticle() {
         return Ref_Article;
     }
 
-    String^ Article::getNom() {
+    string Article::getNom() {
         return Nom_Article;
     }
 
-    String^ Article::getNature() {
+    string Article::getNature() {
         return Nature_Article;
     }
 
-    String^ Article::getCouleur() {
+    string Article::getCouleur() {
         return Couleur_Article;
     }
 
@@ -74,37 +73,38 @@ namespace NS_Article {
         return Supprimer;
     }
 
-    String^ Article::Create() {
-        return "DECLARE @Nature_Article varchar(50); SET @Nature_Article = '" + Nature_Article + "'; "
+    string Article::Create() {
+        return "DECLARE @nature varchar(50); SET @Nature_Article = '" + Nature_Article + "'; "
             "DECLARE @Nom_Article varchar(50); SET @Nom_Article = '" + Nom_Article + "'; "
             "DECLARE @Couleur_Article varchar(50); SET @Couleur_Article  = '" + Couleur_Article + "'; "
+            "DECLARE @Ref_Article  int; SET @Ref_Article = " + to_string(Ref_Article) + "; "
             "IF NOT EXISTS(SELECT Ref_Article FROM Article WHERE Nature_Article = @Nature_Article AND Nom_Article = @Nom_Article AND Couleur_Article = @Couleur_Article) "
-            "BEGIN "
-            "INSERT INTO Article (Nom_Article, Nature_Article, Couleur_Article, Stock_Article, Quantite_Reapprovisionnement, Prix_HT, Taux_TVA, Supprimer) "
-            "VALUES (@Nom_Article, @Nature_Article, @Couleur_Article, " +
-            Convert::ToString(Stock_Article) + ", " + Convert::ToString(Quantite_Reapprovisionnement) + ", " +
-            Convert::ToString(Prix_HT) + ", " + Convert::ToString(Taux_TVA) + ", 0); "
+            "BEGIN"
+            "INSERT INTO Article (Ref_Article, Nom_Article, Nature_Article, Couleur_Article, Stock_Article, Quantite_Reapprovisionnement, Prix_HT, Taux_TVA, Supprimer) "
+            "VALUES (@Ref_Article, @Nom_Article, @Nature_Article, @Couleur_Article, " +
+            to_string(Stock_Article) + ", " + to_string(Quantite_Reapprovisionnement) + ", " +
+            to_string(Prix_HT) + ", " + to_string(Taux_TVA) + ", 0); "
             "SELECT 'L''article a été ajouté avec succès.' AS Resultat; "
-            "END "
-            "ELSE "
-            "BEGIN "
+            "END"
+            "ELSE"
+            "BEGIN"
             "SELECT 'Cet article existe déjà !' AS Resultat; "
             "END";
-    }
-    String^ Article::Update() {
+        }
+
+    string Article::Update() {
         return "UPDATE Article SET Nom_Article = '" + Nom_Article + "', Nature_Article = '" + Nature_Article + "', Couleur_Article = '" + Couleur_Article +
-            "', Stock_Article = " + Stock_Article + ", Quantite_Reapprovisionnement = " +
-            Convert::ToString(Quantite_Reapprovisionnement) + ", Prix_HT = " + Convert::ToString(Prix_HT) +
-            ", Taux_TVA = " + Convert::ToString(Taux_TVA) + ", Supprimer = " + (Supprimer ? "1" : "0") +
-            " WHERE Ref_Article = " + Convert::ToString(Ref_Article);
+            "', Stock_Article = " + to_string(Stock_Article) + "', Quantite_Reapprovisionnement = " +
+            to_string(Quantite_Reapprovisionnement) + ", Prix_HT = " + to_string(Prix_HT) +
+            ", Taux_TVA = " + to_string(Taux_TVA) + ", Supprimer = " + (Supprimer ? "1" : "0") +
+            " WHERE Ref_Article = " + to_string(Ref_Article);
     }
 
-    String^ Article::Delete() {
-        return "UPDATE Article SET Supprimer = 1 WHERE Ref_Article = " + Convert::ToString(Ref_Article);
+    string Article::Delete() {
+        return "UPDATE Article SET supprimer = 1 WHERE Ref_Article = " + to_string(Ref_Article);
     }
 
-    String^ Article::Select() {
+    string Article::Select() {
         return "SELECT * FROM Article";
     }
-
 }
--- Project2/Article.h
+++ Project2/Article.h
@@ -1,16 +1,16 @@
 #pragma once
+#include <string>
+using namespace std;
 
-using namespace System;
-
-namespace NS_Article {
-    ref class Article
+namespace NS_article {
+    class Article
     {
     private:
         int Id_Article;
         int Ref_Article;
-        String^ Nom_Article;
-        String^ Nature_Article;
-        String^ Couleur_Article;
+        string Nom_Article;
+        string Nature_Article;
+        string Couleur_Article;
         int Stock_Article;
         int Quantite_Reapprovisionnement;
         float Prix_HT;
@@ -19,9 +19,9 @@ namespace NS_Article {
 
     public:
         void setRefArticle(int ref_article);
-        void setNom(String^ nom_article);
-        void setNature(String^ nature);
-        void setCouleur(String^ couleur);
+        void setNom(string& nom_article);
+        void setNature(string& nature);
+        void setCouleur(string& couleur);
         void setStock(int stock);
         void setQuantiteReapprovisionnement(int quantite_reapprovisionnement);
         void setPrixHT(float prix_ht);
@@ -29,18 +29,18 @@ namespace NS_Article {
         void setSupprimer(bool supprimer);
 
         int getRefArticle();
-        String^ getNom();
-        String^ getNature();
-        String^ getCouleur();
+        string getNom();
+        string getNature();
+        string getCouleur();
         int getStock();
         int getQuantiteReapprovisionnement();
         float getPrixHT();
         float getTauxTVA();
         bool getSupprimer();
 
-        String^ Create();
-        String^ Update();
-        String^ Delete();
-        String^ Select();
+        string Create();
+        string Update();
+        string Delete();
+        string Select();
     };
 }
